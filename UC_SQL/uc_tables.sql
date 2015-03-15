0094ae12b0c43a8a43067c6aa12886f96dacfd38
Aditya Badrinath <abadrinath15@gmail.com>
2015-03-15 15:13:55 -0400

    Added the functions to load all the cards into db

--- UC_SQL/uc_tables.sql
+++ UC_SQL/uc_tables.sql
@@ -1,14 +1,14 @@
-CREATE TABLE card (
+CREATE TABLE cards (
     card_id INTEGER PRIMARY KEY,
     card_text VarChar(140) NOT NULL,
     theme VarChar(30),
-    num_dealt INTEGER NOT NULL,
-    num_played INTEGER NOT NULL,
+    num_dealt INTEGER NOT NULL DEFAULT 0,
+    num_played INTEGER NOT NULL DEFAULT 0,
     num_not_played INTEGER,
     num_won_round INTEGER,
     num_thumbs_up INTEGER,
     num_thumbs_down INTEGER
-    user_generated BOOLEAN NOT NULL
+    user_generated BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE users (
@@ -53,5 +53,60 @@ CREATE TABLE winning_cards (
     index INTEGER PRIMARY KEY,
     card_id INTEGER NOT NULL,
     game_id INTEGER NOT NULL
-)
+);
+
+INSERT INTO cards (card_text) VALUES
+    ('Stands out like a rasin in a bowl of buttered popcorn.'),
+    ('The Real Slim Shady(tm)'),
+    ('I don''t need to tell you about inappropriate love.'),
+    ('My next step is to search for pictures of
+        naked ladies on my office computer.'),
+    ('Hiding in the closet.'),
+    ('We get it, you''re gay.'),
+    ('Dirty Sanchez'),
+    ('Yellow Fever')
+    ('Shouldn''t you be mowing somebody''s lawn?'),
+    ('Conspiracy theorist'),
+    ('Is that an adam''s apple?!'),
+    ('If it wasn''t for hookers, I would never have met your mother.'),
+    ('Are your eyes always different sizes?'),
+    ('What is it?...'),
+    ('La Chupacabra'),
+    ('Redneck Vampire'),
+    ('I like toy-tules!'),
+    ('Rides the short bus.'),
+    ('He''s not gay, he''s hindu.'),
+    ('Leprechan without gold.'),
+    ('You remind me of 2005...'),
+    ('Gym, Tan, Laundry'),
+    ('The Joisey Shure'),
+    ('If you''re still wearing earrings, you probably peaked in highschool.'),
+    ('Future Catholic Priest'),
+    ('Dibs!'),
+    ('The acid trip that broke the camel''s back'),,
+    ('Danny DeVito'),
+    ('In-cognito big tits'),
+    ('Pee-Wee Herman'),
+    ('The DJ that only plays Eminem and Limp Bizkit'),
+    ('Girls Gone Wild'),
+    ('Andy Dick'),
+    ('Fred Durst'),
+    ('Whitney Houston high on crack'),
+    ('Snookie'),
+    ('“The Situation”'),
+    ('Pauly D.'),
+    ('Chris Angel'),
+    ('Run Forest Run!'),
+    ('Marsha Marsha Marsha'),
+    ('The opposite of James Bond'),
+    ('Catlady'),
+    ('You are soo white, if you ever flew to the middle east you would jump straigh
+        off the plane and into the terrorist''s kidnapping sack.'),
+    ('Steroids!'),
+    ('Undefeated Double Down Challenge Winner'),
+    ('You look like you dye your hair with a bottle of “Soccer Mom No. 45”'),
+    ('All the best cowboys have daddy issues'),
+    ('Do you even lift?');
+
+
 
